c9b652fda5b4549918b80ac14e01b692fb240c41
Vinícius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2020-01-06 17:04:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,8 @@ echo "<h1>Openshift Workshop v2.2</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
 echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
-$conn = new mysqli("mysql", "mysql", "mysql", "mysql");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$result = $conn->query("SELECT nome FROM cidade");
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "<h3>" . $row["nome"] . "</h3>";
-    }
-} else {
+
     echo "0 results";
-}
+
 $conn->close();
 ?>
